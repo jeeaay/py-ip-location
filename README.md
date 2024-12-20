6d93068cfaafbe179bcef65a75acbf527d20d971
jeay <wrjie@msn.cn>
2024-12-20 14:45:56 +0800

    说明

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-#
+# IP address location 通过IP地址获取地理位置
+
+## 简介
+
+Get geographic location through IP address, support IPv4 and IPv6. Combine IP address library and online API. The local IP address library comes from the project [lionsoul2014/ip2region](https://github.com/lionsoul2014/ip2region), and the online API comes from `ip-api` and `ip.sb`.
+
+通过IP地址获取地理位置，支持IPv4和IPv6。结合了IP地址库和在线API。本地的IP地址库来自项目[lionsoul2014/ip2region](https://github.com/lionsoul2014/ip2region)，在线API来自`ip-api`、`ip.sb`。
+
+## How to use 使用方法
+
+```python
+from IP2Reg import IP2Reg
+ip2reg = IP2Reg()
+region = ip2reg.search('8.8.8.8')
+```
--- example1_flask.py
+++ example1_flask.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify, request
 from IP2Reg import IP2Reg
 import json
 
-# pip install flask
+# insstall flask:   pip install flask
+# run:              python example1_flask.py
+# visit:            http://127.0.0.1:5000/ip/<search ip>
 
 app = Flask(__name__)
 
@@ -10,13 +12,14 @@ app = Flask(__name__)
 def home():
     return f"<p>Hello, World!</p>"
 
+
 @app.route("/ip/<ip>")
 def get_ip(ip=None):
-    # jsonp情况
+    # json
     if not request.args.get('callback') or request.args.get('callback').strip() == '':
         return jsonify(IP2Reg(ip).search())
+    # jsonp
     else:
-        print(request.args.get('callback') + json.dumps(IP2Reg(ip).search()))
         return request.args.get('callback') + "(" + json.dumps(IP2Reg(ip).search()) + ")"
 
 if __name__ == "__main__":
